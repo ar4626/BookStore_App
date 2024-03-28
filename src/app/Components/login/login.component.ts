57082ea95db6a32fc1fb9806e28fd759c9fb9b6c
Ankit Raj <ankitsingh17072001@gmail.com>
2024-03-28 10:46:48 +0530

    [Ankit] Add : Created the Ui for the Login page

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  isPasswordVisible: boolean = false;
 
+  toggleVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
 }
