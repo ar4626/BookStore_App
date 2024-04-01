c6eb776e52554d9d6c255c40b78d1d38c72905c9
Ankit Raj <ankitsingh17072001@gmail.com>
2024-04-01 09:39:33 +0530

    [Ankit] Add : Created the UI Structure for Forget password

---
+++ src/app/Components/forget-password/forget-password.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-forget-password',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './forget-password.component.html',
+  styleUrl: './forget-password.component.scss'
+})
+export class ForgetPasswordComponent {
+  forgetForm !: FormGroup;
+
+  constructor(
+    private formbuilder : FormBuilder,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.forgetForm = this.formbuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+      }
+    )
+  }
+}
+ 
