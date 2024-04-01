4539fa86faf175add12255469776504cef37ab61
Ankit Raj <ankitsingh17072001@gmail.com>
2024-04-01 23:43:04 +0530

    [Ankit] Add :  Created the top nacv working on profile menu

---
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
 import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
-        path: '', redirectTo:'reset-password', pathMatch:'full'
+        path: '', redirectTo:'home', pathMatch:'full'
     },
     {
         path: 'login', component: LoginComponent
@@ -19,6 +20,12 @@ export const routes: Routes = [
     },
     {
         path: 'reset-password/:id', component: ResetPasswordComponent
+    },
+    {
+        path: 'home', component: DashboardComponent,
+        children:[
+
+        ]
     },
 ];
  
