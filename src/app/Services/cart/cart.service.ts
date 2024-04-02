77f19242022f344943fc610d8b2640526566a5fa
Ankit Raj <ankitsingh17072001@gmail.com>
2024-04-03 02:27:57 +0530

    [Ankit] Add : Created service for getting all the cart item

--- src/app/Services/cart/cart.service.ts
+++ src/app/Services/cart/cart.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from '../http/http.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  token: any;
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService
+  ) {
+    this.token = typeof localStorage !== 'undefined' ? localStorage.getItem('token') : "";
+  }
+
+  getAllCartItem(){
+    let header = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + this.token
+      })
+    }
+    return this.httpService.getServiceToken('https://localhost:44362/api/Cart/GetCartItems',true,header);
+  }
 }
